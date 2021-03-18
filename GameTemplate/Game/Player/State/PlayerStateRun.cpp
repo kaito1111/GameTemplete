4c2f6a1b33f031e247e3563e005c90a6f1bb8b14
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2021-03-18 23:44:28 +0900

    書いて消した（へんかなし）

--- GameTemplate/Game/Player/State/PlayerStateRun.cpp
+++ GameTemplate/Game/Player/State/PlayerStateRun.cpp
@@ -5,7 +5,8 @@
 void PlayerStateRun::Update() {
 	m_player->SetMulAnimSpeed(1.0f);
 	m_player->PlayAnimation(Player::walk);
-	m_player->TryChangIdleState();
+	
+	m_player->TryChangeIdleState();
 	m_player->TryChangeBackStepState();
 	m_player->TryChangeAttackState();
 	m_player->TryChangeRollingAttackState();
