3421db3ce11aec90eaf4ead3303195aea8f08fb5
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2021-04-14 18:47:49 +0900

    ボスのアニメーションを追加

--- GameTemplate/Game/Boss/Boss.cpp
+++ GameTemplate/Game/Boss/Boss.cpp
@@ -7,7 +7,7 @@ bool Boss::Start() {
 	m_Model->SetRotation(m_Rot);
 	m_Model->SetScale(CVector3::One()*1.8f);
 
-	m_AnimationClip[Walk].Load(L"Assets/animData/BossWalk.tka");
+	m_AnimationClip[Walk].Load(L"Assets/animData/BossDy.tka");
 	m_Animation.Init(m_Model->GetModel(), m_AnimationClip, StateNum);
 	return true;
 }
