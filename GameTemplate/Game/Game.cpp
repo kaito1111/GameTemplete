50f1ab3de7fe65da87cfd6f6c64ff5a56de9697c
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2021-04-26 00:40:42 +0900

    プレイヤーのスポーン位置をレベルに合わせる

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -18,7 +18,7 @@ bool Game::Start()
 	m_Stage = NewGO<Stage>(0);
 	m_Stage->Load(m_LevelFilePath);
 	m_Player = NewGO<Player>(1, "player");
-	m_Player->SetSpownPos(CVector3::Zero());
+	m_Player->SetSpownPos(m_Stage->GetPlayerPos());
 	m_Player->SetSpwonHp(m_SpownHp);
 	m_Camera = NewGO<GameCamera>(0);
 	m_EnemySpawner = NewGO<EnemySpawner>(0);
