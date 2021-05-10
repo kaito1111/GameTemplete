3ce11e1b3161ea6e7b6accdfc3312bf3a1264f1c
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2021-05-10 16:50:24 +0900

    a

---
+++ GameTemplate/Game/Archer/State/ArcherDethState.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Archer/State/IArcherState.h"
+class Archer;
+class ArcherDethState :
+	public IArcherState
+{
+public :
+	ArcherDethState(Archer* ac) :
+		IArcherState(ac) {
+
+	}
+	~ArcherDethState() {}
+
+	void Update();
+	bool IsPossibleHpDown() {
+		return false;
+	}
+};
+
