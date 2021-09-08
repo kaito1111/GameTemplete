90d05e4199edc172771722c06fa44f247187457a
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2021-09-08 15:33:26 +0900

    リファクタリング

--- GameTemplate/Game/Stage/candle.h
+++ GameTemplate/Game/Stage/candle.h
@@ -2,16 +2,46 @@
 class Candle: public IGameObject
 {
 public:
-	Candle() {};
-	~Candle() {};
 
+	/// <summary>
+	/// インスタンスが生成されると、一度だけ呼ばれる開始処理
+	/// </summary>
+	/// <remarks>
+	/// UnityのStart()関数の仕様に準拠。
+	/// </remarks>
+	/// <returns>
+	/// trueが帰ってきたら、初期化完了。
+	/// 複数フレームにわたって初期化をしたい場合は、
+	/// 初期化完了までfalseを返す。
+	/// </returns>
 	bool Start()override final;
+	/// <summary>
+	/// 毎フレーム呼ばれる更新処理。
+	/// </summary>
 	void Update()override final;
+	/// <summary>
+	/// インスタンスが破棄される時に呼ばれる関数。
+	/// </summary>
+	/// <remarks>
+	/// 本エンジンで実装している、ゲームオブジェクトを削除する
+	/// DeleteGO関数は、すぐにインスタンスを削除するわけではなく、
+	/// 1フレーム遅れてインスタンスが削除される。
+	/// そのため、デストラクタの呼び出しが、DeleteGOの呼び出しから1フレーム遅れることとなる。
+	/// DeleteGOが呼ばれたタイミングで、行いたい終了処理はOnDestroy()に記述する。
+	/// </remarks>
 	void OnDestroy()override final;
 
+	/// <summary>
+	/// モデル位置を設定
+	/// </summary>
+	/// <param name="pos">モデルの位置</param>
 	void SetPosition(const CVector3& pos) {
 		m_Position = pos;
 	}
+	/// <summary>
+	/// モデルの回転を設定
+	/// </summary>
+	/// <param name="rot"></param>
 	void SetRotation(const CQuaternion& rot) {
 		m_Rotation = rot;
 	}
